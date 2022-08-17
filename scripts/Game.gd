0e01c95cf9ef476232da519ef3e6be75bc8223dd
SHyguymoll <shymollet@gmail.com>
2022-08-16 17:34:17 -0700

    finished fixing state business and camera, attempted fix for animation

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -19,10 +19,10 @@ const movementBoundX = 8
 func startGame():
 	player1.translation = Vector3(player1.STARTXOFFSET * -1,0,0)
 	player1.rightFacing = true
-	player1.stateCurrent = "Idle"
+	player1.stateCurrent = statesBase.Idle
 	player2.translation = Vector3(player2.STARTXOFFSET,0,0)
 	player2.rightFacing = false
-	player2.stateCurrent = "Idle"
+	player2.stateCurrent = statesBase.Idle
 
 func _ready():
 	stage = load("res://Content/Game/Stages/BlankStage.tscn")
@@ -31,8 +31,6 @@ func _ready():
 	player2 = load(get_node("/root/CharactersDict").player2.tscnFile).instance()
 	add_child(player1)
 	add_child(player2)
-	player1.loadExtraData()
-	player2.loadExtraData()
 	startGame()
 
 func cameraControl(mode: int):
@@ -46,8 +44,9 @@ func cameraControl(mode: int):
 			$Camera.translation.y += 1
 			$Camera.translation.x = clamp($Camera.translation.x, -cameraMaxX, cameraMaxX)
 			$Camera.translation.y = clamp($Camera.translation.y, 0, cameraMaxY)
-			$Camera.translation.z = clamp(abs(player1.translation.x-player2.translation.x)/2, 1.5, 1.825)
-			$Camera.translation.z += 0.5
+			#$Camera.translation.z = clamp(abs(player1.translation.x-player2.translation.x)/2, 1.5, 1.825)
+			$Camera.translation.z = 1.328125
+			$Camera.size = clamp(abs(player1.translation.x-player2.translation.x)/2, 3, 4)
 #			$Camera.translation.z = abs(player1.translation.x-player2.translation.x)/2
 		1: #focus player1
 			$Camera.translation.x = (player1.translation.x+(player2.translation.x * 0.05))/2
@@ -80,24 +79,42 @@ func handleInputs():
 	player1.handleInput()
 	player2.handleInput()
 
+const HALFPI = 180
+const TURNAROUND_ANIMSTEP = 3
 
 func characterActBasic():
 	if player1.stateCurrent in statesBase:
 		if player1.translation < player2.translation:
-			player1.rightFacing = true
-			if player1.stateCurrent == statesBase.Idle:
-				player1.animStep = 3
-			if player1.stateCurrent == statesBase.Crouch:
-				player1.animStep = 3
+			if player1.rightFacing != true:
+				player1.rightFacing = true
+				if player1.stateCurrent == statesBase.Idle:
+					player1.animStep = TURNAROUND_ANIMSTEP
+				if player1.stateCurrent == statesBase.Crouch:
+					player1.animStep = TURNAROUND_ANIMSTEP
 		else:
-			player1.rightFacing = false
-		player1.rotation_degrees.y = 180 * int(player1.rightFacing)
+			if player1.rightFacing != false:
+				player1.rightFacing = false
+				if player1.stateCurrent == statesBase.Idle:
+					player1.animStep = TURNAROUND_ANIMSTEP
+				if player1.stateCurrent == statesBase.Crouch:
+					player1.animStep = TURNAROUND_ANIMSTEP
+		player1.rotation_degrees.y = HALFPI * int(player1.rightFacing)
 	if player2.stateCurrent in statesBase:
 		if player2.translation < player1.translation:
-			player2.rightFacing = true
+			if player2.rightFacing != true:
+				player2.rightFacing = true
+				if player2.stateCurrent == statesBase.Idle:
+					player2.animStep = TURNAROUND_ANIMSTEP
+				if player2.stateCurrent == statesBase.Crouch:
+					player2.animStep = TURNAROUND_ANIMSTEP
 		else:
-			player2.rightFacing = false
-		player2.rotation_degrees.y = 180 * int(player2.rightFacing)
+			if player2.rightFacing != false:
+				player2.rightFacing = false
+				if player2.stateCurrent == statesBase.Idle:
+					player2.animStep = TURNAROUND_ANIMSTEP
+				if player2.stateCurrent == statesBase.Crouch:
+					player2.animStep = TURNAROUND_ANIMSTEP
+		player2.rotation_degrees.y = HALFPI * int(player2.rightFacing)
 	player1.translation.x = clamp(player1.translation.x, -movementBoundX, movementBoundX)
 	player1.translation.y = max(0.0, player1.translation.y)
 	player2.translation.x = clamp(player2.translation.x, -movementBoundX, movementBoundX)
