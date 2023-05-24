7a509226a955a6628c989fc265e4ca5142f72fd5
SHyguymoll <shymollet@gmail.com>
2023-05-23 18:31:44 -0700

    parity with FightingGameCharacter repo

--- scripts/InputHandle.gd
+++ scripts/InputHandle.gd
@@ -3,7 +3,19 @@ extends Node
 var p1_buttons = [false, false, false, false, false, false, false, false, false, false]
 var p2_buttons = [false, false, false, false, false, false, false, false, false, false]
 
-var p1_inputs : Array = []
-var p2_inputs : Array = []
+var p1_inputs : Dictionary = {
+	up=[[0, false]],
+	down=[[0, false]],
+	left=[[0, false]],
+	right=[[0, false]],
+}
+
+var p2_inputs : Dictionary = {
+	up=[[0, false]],
+	down=[[0, false]],
+	left=[[0, false]],
+	right=[[0, false]],
+}
+
 var p1_input_index : int = 0
 var p2_input_index : int = 0
