a39131bccf5c2034ff11882b9ea18e6423575daa
SHyguymoll <shymollet@gmail.com>
2024-02-27 21:30:21 -0800

    added missing projectile script reference
    However, none of what I've tried to do so far with being able to load in fighters dynamically have bared healthy fruit. From now on I'm going to rework things to be a single project that the user would export through godot in a much simpler manner.

---
+++ scripts/ReferenceScripts/projectile.gd
@@ -0,0 +1,32 @@
+class_name Projectile
+extends CharacterBody3D
+
+signal projectile_ended(proj)
+
+@export var hitbox : Node3D
+var right_facing : bool
+var type : int
+var source : int
+
+func _ready():
+	pass
+
+func tick():
+	pass
+
+func destroy():
+	if hitbox != null:
+		hitbox.queue_free()
+	velocity = Vector3.ZERO
+
+func _on_projectile_contact(other):
+	if hitbox == null:
+		return
+	var o_par = other.get_parent()
+	if other is Stage or o_par is Stage:
+		destroy()
+	if o_par is Projectile:
+		if o_par.hitbox == null:
+			return
+		if source != o_par.source and hitbox.hit_priority <= o_par.hitbox.hit_priority:
+			destroy()
