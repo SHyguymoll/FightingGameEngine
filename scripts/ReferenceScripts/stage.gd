b8f742f8cf753ff5cea6fd0f7e6b233c49359e31
SHyguymoll <shymollet@gmail.com>
2024-03-18 21:31:21 -0700

    stage variables are now exported

--- scripts/ReferenceScripts/stage.gd
+++ scripts/ReferenceScripts/stage.gd
@@ -1,8 +1,8 @@
 class_name Stage
 extends StaticBody3D
 
-var stage_name : String = "Blank 2D Stage"
+@export var stage_name : String
 enum modes {
 	two_d, three_d, other
 }
-var mode : modes = modes.two_d
+@export var mode : modes
