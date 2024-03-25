3672ff15b01bbaedeb8a457340824a99c0f92baf
SHyguymoll <shymollet@gmail.com>
2024-03-25 12:10:32 -0700

    rewrote stage variables

--- scripts/ReferenceScripts/stage.gd
+++ scripts/ReferenceScripts/stage.gd
@@ -2,7 +2,5 @@ class_name Stage
 extends StaticBody3D
 
 @export var stage_name : String
-enum modes {
-	two_d, three_d, other
-}
-@export var mode : modes
+enum Modes {TWO_D, THREE_D, OTHER}
+@export var mode : Modes
